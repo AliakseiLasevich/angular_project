ce9b37a699f3152323b17031e12712aa42a1425e
Aliaksei Lasevich <alexey.losevich@gmail.com>
2022-09-22 15:25:14 +0200

    10. Add dynamically class changing with directive @HostListener

--- src/app/directive/favorite.directive.ts
+++ src/app/directive/favorite.directive.ts
@@ -1,12 +1,19 @@
-import {Directive, HostBinding, Input} from "@angular/core";
+import {Directive, HostBinding, HostListener, Input} from "@angular/core";
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
 
-  @Input() set mwFavorite(value: boolean) {
+  @HostListener('mouseenter') onMouseEnter(){
+    this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.hovering = false;
+  }
+  @Input() set changeHovering(value: boolean) {
     this.isFavorite = value;
   }
 }
