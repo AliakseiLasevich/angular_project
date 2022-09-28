6a38d59a61e185e282e9155e8ae9f5a3ebf93487
Aliaksei Lasevich <alexey.losevich@gmail.com>
2022-09-28 16:18:31 +0200

    1. initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { ButtonComponent } from './components/common/button/button.component';
+import { HeaderComponent } from './components/container/header/header.component';
+import { BodyComponent } from './components/container/body/body.component';
+import { FooterComponent } from './components/container/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ButtonComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
